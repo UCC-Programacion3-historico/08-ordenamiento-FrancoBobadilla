0cf4d9c8ae2507b5eff62f7110a78adb0e899d84
FrancoBobadilla <30866005+FrancoBobadilla@users.noreply.github.com>
2017-11-02 20:40:19 -0300

    solo falta interSort

--- InterSort.cpp
+++ InterSort.cpp
@@ -1,6 +1,35 @@
 #include "InterSort.h"
 
-template <class T>
-void interSort(T *vect, unsigned long tamanio){
-
+template<class T>
+void interSort(T *vect, unsigned long tamanio) {
+    int incr = 1;
+    while (incr < tamanio) {
+        incr *= 2;
+        for (int i = 0; i < tamanio - 1; i += incr)
+            if (incr + i <= tamanio) {
+                bool intercambios = true;
+                for (int j = 1; j < incr && intercambios; ++j) {
+                    intercambios = false;
+                    for (int k = i; k < incr - j; ++k)
+                        if (vect[k] > vect[k + 1]) {
+                            intercambios = true;
+                            T comodin = vect[k];
+                            vect[k] = vect[k + 1];
+                            vect[k + 1] = comodin;
+                        }
+                }
+            } else {
+                bool intercambios = true;
+                for (int j = 1; j < tamanio - 1 && intercambios; ++j) {
+                    intercambios = false;
+                    for (int k = i; k < tamanio - i - j; ++k)
+                        if (vect[k] > vect[k + 1]) {
+                            intercambios = true;
+                            T comodin = vect[k];
+                            vect[k] = vect[k + 1];
+                            vect[k + 1] = comodin;
+                        }
+                }
+            }
+    }
 }
